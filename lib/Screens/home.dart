2fbe4b4d67099c7151504db1ccda3bdbd3bd55d2
mariyajose070@gmail.com <mariyajose070@gmail.com>
2024-04-30 14:57:01 +0530

    store item

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,35 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:initial_flutter_project/Screens/login.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:flutter/material.dart';
+// import 'package:initial_flutter_project/Screens/login.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-class ScreeHome extends StatelessWidget {
-  const ScreeHome({super.key});
+// class ScreeHome extends StatelessWidget {
+//   const ScreeHome({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                signout(context);
-              },
-              icon: Icon(Icons.exit_to_app))
-        ],
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Text('Home'),
-        ),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Home'),
+//         actions: [
+//           IconButton(
+//               onPressed: () {
+//                 signout(context);
+//               },
+//               icon: Icon(Icons.exit_to_app))
+//         ],
+//       ),
+//       body: SafeArea(
+//         child: Center(
+//           child: Text('Home'),
+//         ),
+//       ),
+//     );
+//   }
 
-  signout(BuildContext cxt) async {
-    final _sharedPreferance = await SharedPreferences.getInstance();
-    await _sharedPreferance.clear();
-    Navigator.of(cxt).pushAndRemoveUntil(
-        MaterialPageRoute(builder: (ctx1) => ScreenLogin()), (route) => false);
-  }
-}
+//   signout(BuildContext cxt) async {
+//     final _sharedPreferance = await SharedPreferences.getInstance();
+//     await _sharedPreferance.clear();
+//     Navigator.of(cxt).pushAndRemoveUntil(
+//         MaterialPageRoute(builder: (ctx1) => ScreenLogin()), (route) => false);
+//   }
+// }
--- lib/Screens/splash.dart
+++ lib/Screens/splash.dart
@@ -1,62 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:initial_flutter_project/Screens/home.dart';
-import 'package:initial_flutter_project/Screens/login.dart';
-import 'package:initial_flutter_project/main.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:flutter/material.dart';
+// import 'package:initial_flutter_project/Screens/home.dart';
+// import 'package:initial_flutter_project/Screens/login.dart';
+// import 'package:initial_flutter_project/main.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-class ScreenSplash extends StatefulWidget {
-  const ScreenSplash({super.key});
+// class ScreenSplash extends StatefulWidget {
+//   const ScreenSplash({super.key});
 
-  @override
-  State<ScreenSplash> createState() => _ScreenSplashState();
-}
+//   @override
+//   State<ScreenSplash> createState() => _ScreenSplashState();
+// }
 
-class _ScreenSplashState extends State<ScreenSplash> {
-  @override
-  void initState() {
-    checkUserLoggIn();
-    super.initState();
-  }
+// class _ScreenSplashState extends State<ScreenSplash> {
+//   @override
+//   void initState() {
+//     checkUserLoggIn();
+//     super.initState();
+//   }
 
-  @override
-  void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
-    super.didChangeDependencies();
-  }
+//   @override
+//   void didChangeDependencies() {
+//     // TODO: implement didChangeDependencies
+//     super.didChangeDependencies();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset('assets/images/orange.jpg'),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Image.asset('assets/images/orange.jpg'),
+//       ),
+//     );
+//   }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     // TODO: implement dispose
+//     super.dispose();
+//   }
 
-  Future<void> gotoLogin() async {
-    await Future.delayed(Duration(seconds: 3));
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: (ctx) => ScreenLogin(),
-      ),
-    );
-  }
+//   Future<void> gotoLogin() async {
+//     await Future.delayed(Duration(seconds: 3));
+//     Navigator.of(context).pushReplacement(
+//       MaterialPageRoute(
+//         builder: (ctx) => ScreenLogin(),
+//       ),
+//     );
+//   }
 
-  Future<void> checkUserLoggIn() async {
-    final _sharedPrefs = await SharedPreferences.getInstance();
-    _sharedPrefs.get(SAVE_KEY_NAME);
-    final _userLoggedIn = _sharedPrefs.getBool(SAVE_KEY_NAME);
-    if (_userLoggedIn == null || _userLoggedIn == false) {
-      gotoLogin();
-    } else {
-      Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (ctx1) => ScreeHome()));
-    }
-  }
-}
+//   Future<void> checkUserLoggIn() async {
+//     final _sharedPrefs = await SharedPreferences.getInstance();
+//     _sharedPrefs.get(SAVE_KEY_NAME);
+//     final _userLoggedIn = _sharedPrefs.getBool(SAVE_KEY_NAME);
+//     if (_userLoggedIn == null || _userLoggedIn == false) {
+//       gotoLogin();
+//     } else {
+//       Navigator.of(context)
+//           .pushReplacement(MaterialPageRoute(builder: (ctx1) => ScreeHome()));
+//     }
+//   }
+// }
